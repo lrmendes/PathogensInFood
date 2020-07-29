0ac43ae8b7a8c7a4f6a4286e8518efabef307de7
lrmendes <lrmen14@gmail.com>
2020-07-29 06:31:28 +0100

    Updates

---
+++ precache-manifest.b445cf6bf7f0c3e7e220dd43488cd7a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0202240b092715f690575d1a148edb08",
+    "url": "/PathogensInFood/index.html"
+  },
+  {
+    "revision": "fb4cbe1bf5785c886ad0",
+    "url": "/PathogensInFood/static/js/2.e5895402.chunk.js"
+  },
+  {
+    "revision": "7917da86db085bfd328c952c2c795b8b",
+    "url": "/PathogensInFood/static/js/2.e5895402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fcf33b21531a8539198",
+    "url": "/PathogensInFood/static/js/main.5af090cd.chunk.js"
+  },
+  {
+    "revision": "1ce10ab1263c632f69ce",
+    "url": "/PathogensInFood/static/js/runtime-main.48c3daed.js"
+  },
+  {
+    "revision": "8f08db80b7b1b4101ccff1f06a643331",
+    "url": "/PathogensInFood/static/media/bacteria.8f08db80.svg"
+  },
+  {
+    "revision": "7204ad86df2a6ad6333905ed1c3f398a",
+    "url": "/PathogensInFood/static/media/cimo_bg_menu2.7204ad86.jpg"
+  },
+  {
+    "revision": "aa9dbbc8501276946253dae518359638",
+    "url": "/PathogensInFood/static/media/cimo_logo.aa9dbbc8.svg"
+  },
+  {
+    "revision": "52b596c4e746e78d05e090da774bb310",
+    "url": "/PathogensInFood/static/media/ipb_logo.52b596c4.svg"
+  },
+  {
+    "revision": "6d336348d9d52d34e506b7220ed46bd1",
+    "url": "/PathogensInFood/static/media/logo_menu.6d336348.png"
+  },
+  {
+    "revision": "9b51008ff40676b8146fae71d4889810",
+    "url": "/PathogensInFood/static/media/parasite.9b51008f.svg"
+  },
+  {
+    "revision": "b4875b5bc76836edbb5a64247487d769",
+    "url": "/PathogensInFood/static/media/virus.b4875b5b.svg"
+  }
+]);
