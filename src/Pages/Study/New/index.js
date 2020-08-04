b437f944ef007accfab659d739757f61d6ff8e7e
lrmendes <lrmen14@gmail.com>
2020-08-04 14:56:09 +0100

    Register Study Page Updates

--- src/Pages/Study/New/index.js
+++ src/Pages/Study/New/index.js
@@ -7,9 +7,24 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 import { Scope } from "@unform/core";
 import { Form } from "@unform/web";
+
+import {
+  TextField,
+  RadioGroup,
+  FormControl,
+  FormControlLabel,
+  Radio,
+  Box,
+  FormLabel,
+  Snackbar,
+} from "@material-ui/core";
+
+import MuiAlert from '@material-ui/lab/Alert';
+import SendIcon from '@material-ui/icons/Send';
 
 import MaterialInput from "../../../Components/Inputs/MaterialInput";
 import MaterialNativeSelect from "../../../Components/Selects/MaterialNativeSelect";
@@ -18,6 +33,10 @@ import Colors from "../../../Styles/Colors";
 import Texts from "../../../Styles/Texts";
 
 import * as Yup from 'yup';
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -55,12 +74,22 @@ const useStyles = makeStyles((theme) => ({
   formWide: {
     width: "100%",
   },
+  input: {
+    display: 'none',
+  },
 }));
 
 export default function BacteriaNew() {
   const blank_text = Texts.leave_blank;
+  const required_text = Texts.required;
   const blank_text_error = Texts.blank_error;
-  const formRef = useRef(null);
+  
+  const formType0Ref = useRef(null);
+  const formType1Ref = useRef(null);
+
+  const [rawFile, setRawFile] = useState(null);
+  const [workFile, setWorkFile] = useState({});
+
 
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
@@ -69,65 +98,162 @@ export default function BacteriaNew() {
   const jsonGeneralInfo = json_general_info;
   const [dataGeneralInfo, setDataGeneralInfo] = useState({});
 
-  async function handleSubmit(data, { reset }) {
+  const [registerType,setRegisterType] = useState("0");
+  const [openSnackAlert,setOpenSnackAlert] = useState(false);
 
-    //console.log(Yup.object());
+  async function handleSubmitFormType0(data, { reset }) {
     console.log(data);
-    
-    let resErrors = {}
-    Object.keys(data).map((value,index) => {
-        //console.log(value,"->",formRef.current.getFieldRef(value));
-        //console.log(typeof formRef.current.getFieldRef(value));
-        //console.log(value,"->",);
-        //console.log("-----");
 
-        let isTextField = formRef.current.getFieldRef(value).current || formRef.current.getFieldRef(value).props || true;
-        //console.log(value," -> ",isTextField);
+    let resErrors = {}
+    let haveErrors = false;
+    Object.keys(data).map((value,index) => {
+        let isTextField = formType0Ref.current.getFieldRef(value).current || formType0Ref.current.getFieldRef(value).props || true;
 
         if (typeof isTextField != 'boolean') {
-          if (data[value] == "") {
-            formRef.current.setFieldError(value, true);
+          if (data[value] === "") {
+            formType0Ref.current.setFieldError(value, true);
+            haveErrors = true;
             resErrors[value] = Yup.string().required();
-            
           } else {
-            formRef.current.setFieldError(value, false);
+            formType0Ref.current.setFieldError(value, false);
           }
         } else {
-          //console.log("Nao é Dropdown: ",value);
-          //console.log(typeof isTextField)
+          if (data[value] === "") {
+            if (formType0Ref.current.getFieldRef(value).required) {
+              formType0Ref.current.setFieldError(value, true);
+              haveErrors = true;
+              resErrors[value] = Yup.string().required();
+            }
+          } else {
+            formType0Ref.current.setFieldError(value, false);
+          }
         }
     });
+
+    if (!haveErrors) {
+      console.log("Registrado com sucesso!");
+    } else {
+      setOpenSnackAlert(true);
+    }
   }
 
-  const handleGeneralInfo = (key, value) => {
-    if (value == "null") {
-      value = null;
+  const handleRegisterType = (event) => {
+    setRegisterType(event.target.value);
+  }
+
+  const handleFileVariables = () => {
+    setWorkFile({});
+
+    let raw = rawFile;
+    let rawLine = raw.split(/\n/);
+    for (let i in rawLine) {
+      if (rawLine[i].includes('=')) {
+        let varValue = rawLine[i].split('=')[1];
+        let varName = rawLine[i].split('=')[0].trim();  
+        //console.log(varName," -> ",varValue);
+        setWorkFile({...workFile, [varName]: varValue});
+      }
     }
-    setDataGeneralInfo({ ...dataGeneralInfo, [key]: value });
-  };
+  }
 
-  const validateForm = () => {
-    let finalJson = {};
-
-    //console.log("General Info:",dataGeneralInfo,"Size: ",dataAux.length);
-    Object.keys(jsonGeneralInfo).map((key) => {
-      finalJson[key] = dataGeneralInfo[key] || "NA";
-    });
-
-    //console.log("\n\n\nFIM:");
-
-    console.log(finalJson);
-  };
-
-  const formatLabel = (label) => {
-    label = label.split("_").join(" ");
-    return label.charAt(0).toUpperCase() + label.slice(1)
+  const handleFileChosen = (file) => {
+    if (file === undefined || file === null) {
+      setRawFile(null);
+      return null;
+    }
+    
+    let fileReader = new FileReader();
+    fileReader.onloadend = (read) => {
+      //this.setState({filetext : read.result});
+      console.log(read.currentTarget.result);
+      setRawFile(read.currentTarget.result);
+    }
+    fileReader.readAsText(file);
   }
 
   return (
       <Grid container spacing={3}>
       <Grid item xs={12} md={12}>
-      <Form ref={formRef} className={classes.formWide} onSubmit={handleSubmit}>
+        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>Register Type</Typography>
+        <Divider />
+        <Paper className={fixedHeightPaper}>
+          <FormControl component="fieldset" className={classes.formControl}>
+          <RadioGroup row aria-label="essayType" name="registerType" value={registerType} onChange={handleRegisterType}>
+            <FormControlLabel value="0" selected control={<Radio />} label="From BibTex" />
+            <FormControlLabel value="1" control={<Radio />} label="Manual Entry" />
+          </RadioGroup>
+          </FormControl>
+        </Paper>
+      </Grid>
+      {registerType === "0" 
+      ? 
+      <Grid item xs={12} md={12}>
+        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>Upload File (only bib/txt)</Typography>
+        <Divider />
+        <Paper className={fixedHeightPaper}>
+        <Grid container>
+        <div>
+          <input
+            accept=".bib,.txt"
+            className={classes.input}
+            id="contained-button-file"
+            type="file"
+            onChange={e => handleFileChosen(e.target.files[0])}
+          />
+          <label htmlFor="contained-button-file">
+          <Button
+            variant="contained"
+            color="default"
+            component="span"
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload
+          </Button>
+          </label>
+        </div>
+        <Box ml={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<SendIcon />}
+            disabled={rawFile === null ? true : false}
+            onClick={handleFileVariables}
+          >
+          Read File
+          </Button>
+        </Box>
+        </Grid>
+        </Paper>
+
+      <Grid item xs={12} md={12}>
+      <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>Study Info</Typography>
+      <Divider />
+      <Paper className={fixedHeightPaper}>
+      { 
+          Object.entries(workFile).map(([key, value]) => {
+            return value.data == null 
+            ?
+            <MaterialInput
+            key={key}
+            name={key}
+            label={value}
+            type={"text"}
+            isrequired={true}
+            labelError={blank_text_error}
+            placeholder={"enter text..."}
+          />
+          : null
+      })}
+      </Paper>
+      </Grid>
+
+      </Grid>
+      : null
+      }
+      {registerType === "1"
+      ? 
+      <Grid item xs={12} md={12}>
+      <Form ref={formType0Ref} className={classes.formWide} onSubmit={handleSubmitFormType0}>
       <Grid container spacing={3}>
           <Grid item xs={12} md={12}>
             <Typography
@@ -142,38 +268,42 @@ export default function BacteriaNew() {
             <Divider />
             {/*console.log("RODOU")*/}
             <Paper className={fixedHeightPaper}>
-              {Object.keys(jsonGeneralInfo).map((json, index) => {
-                return jsonGeneralInfo[json] == null ? (
-                  <MaterialInput
-                    key={json}
-                    name={json}
-                    label={formatLabel(json)}
-                    placeholder={"enter text..."}
-                  />
-                ) : (
-                    <MaterialNativeSelect
-                      key={json}
-                      label={formatLabel(json)}
-                      labelError={blank_text_error}
-                      defaultValue={""}
-                      name={json}
-                    >
-                      <option value={""}>Select</option >
-                      {Object.keys(jsonGeneralInfo[json]).map((key) =>
-                        key != "label" && key != "selected" ? (
-                          <option key={key} value={key}>
-                            {key}
-                          </option >
-                        ) : null
-                      )}
-                    </MaterialNativeSelect>
-                  );
-              })}
+            { 
+              Object.entries(jsonGeneralInfo).map(([key, value]) => {
+                return value.data == null 
+                ?
+              <MaterialInput
+                key={key}
+                name={key}
+                label={value.label + (value.required ? required_text : blank_text)}
+                type={value.type || "text"}
+                isrequired={value.required}
+                labelError={blank_text_error}
+                placeholder={"enter text..."}
+              />
+              :
+              <MaterialNativeSelect
+              key={key}
+              label={value.label + (value.required ? required_text : blank_text)}
+              defaultValue={""}
+              labelError={blank_text_error}
+              name={key}
+              >
+              <option value={""}>Select</option >
+              {value.data.map((dataValue) =>
+                dataValue != "label" && dataValue != "selected" ? (
+                  <option key={dataValue} value={dataValue}>
+                    {dataValue}
+                  </option >
+                ) : null
+              )}
+              </MaterialNativeSelect>
+            })}
             </Paper>
           </Grid>
           <Grid item xs={12} md={12} container justify="flex-end">
               <Button
-                type="submit"
+                onClick={() => formType0Ref.current.submitForm()}
                 variant="contained"
                 size="large"
                 className={classes.customBTN}
@@ -183,7 +313,15 @@ export default function BacteriaNew() {
             </Grid>
       </Grid>
       </Form>
-    </Grid>
+      </Grid>
+      : null}
+
+      <Snackbar open={openSnackAlert} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} autoHideDuration={3000} onClose={() => setOpenSnackAlert(false)}>
+        <Alert onClose={() => setOpenSnackAlert(false)} severity="error">
+          ERROR: Check that all required fields have been completed.
+        </Alert>
+      </Snackbar>
+
     </Grid>
   );
 }
